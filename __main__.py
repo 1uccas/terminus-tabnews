a1e6ee16a4eda60f96439e6b1eb717e7d37ffa95
Luccas <nascimentolucas2164@gmail.com>
2024-06-21 17:21:42 -0300

    refactor --> update title in Table

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ import feedparser
 from rich.console import Console
 from rich.table import Table
 
-table = Table(title="Tabnews")
+table = Table(title="TabNews: Conteúdos para quem trabalha com Programação e Tecnologia")
 table.show_lines=True
 table.add_column("title", justify="center")
 table.add_column("link", justify="center")
