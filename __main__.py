257fb9a4eb23c7861ad66d62ef8fea015bf6c759
Luccas <nascimentolucas2164@gmail.com>
2024-06-21 16:44:31 -0300

    feat --> add column from table
    pip install rich

--- __main__.py
+++ __main__.py
@@ -1,4 +1,11 @@
 import feedparser
+from rich.console import Console
+from rich.table import Table
+
+table = Table(title="Tabnews")
+table.show_lines=True
+table.add_column("title", justify="center")
+table.add_column("link", justify="center")
 
 url = feedparser.parse('https://www.tabnews.com.br/recentes/rss')
 print(url.channel.title)
