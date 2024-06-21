81219c6a02accfb2dc494747e3d850c189d263f3
Luccas <nascimentolucas2164@gmail.com>
2024-06-21 18:19:29 -0300

    refactor --> add console on top

--- __main__.py
+++ __main__.py
@@ -2,18 +2,17 @@ import feedparser
 from rich.console import Console
 from rich.table import Table
 
+console = Console()
+
 table = Table(title="TabNews: Conteúdos para quem trabalha com Programação e Tecnologia")
 table.show_lines=True
 table.add_column("title", justify="center")
 table.add_column("link", justify="center")
 
 url = feedparser.parse('https://www.tabnews.com.br/recentes/rss')
-
-
 lenn = len(url.entries)
 
 for i in range (0, lenn):
     table.add_row(f"{url.entries[i].title}",f"{url.entries[i].link}")
 
-console = Console()
 console.print(table)
