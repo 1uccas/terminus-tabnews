a50a030116855f9385774c8c250037b250f6d7d9
Luccas <nascimentolucas2164@gmail.com>
2024-06-21 16:23:25 -0300

    feat --> add entries view

--- __main__.py
+++ __main__.py
@@ -1,4 +1,11 @@
 import feedparser
 
 url = feedparser.parse('https://www.tabnews.com.br/recentes/rss')
-print(url.feed.title)
+print(url.channel.title)
+
+lenn = len(url.entries)
+
+for i in range (1, lenn):
+    print(f"\n{url.entries[i].title}\n")
+    print(f"{url.entries[i].description}\n")
+    print(f"{url.entries[i].link}")
