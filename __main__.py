9e57e550143fce7c9613c3260e4a6d9d4858b1c7
Luccas <nascimentolucas2164@gmail.com>
2024-06-24 18:00:36 -0300

    feat --> remove def init () and put while True in the code scope
    * Create new def clear()

--- __main__.py
+++ __main__.py
@@ -2,35 +2,41 @@ import feedparser
 from rich.console import Console
 from rich.table import Table
 import webbrowser as web
+import os
 
 console = Console()
 
-table = Table()
-table.show_lines=True
-table.add_column("Item", justify="center")
-table.add_column("title", justify="center")
-table.add_column("link", justify="center")
 
-url = feedparser.parse('https://www.tabnews.com.br/recentes/rss')
-lenn = len(url.entries)
 
-def init():
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+while True:
+    table = Table()
+    table.show_lines=True
+    table.add_column("Item", justify="center")
+    table.add_column("title", justify="center")
+    table.add_column("link", justify="center")
+
+    url = feedparser.parse('https://www.tabnews.com.br/recentes/rss')
+    lenn = len(url.entries)
+    
     for i in range (0, lenn):
         table.add_row(f"{i}",f"{url.entries[i].title}",f"{url.entries[i].link}")
 
     console.print(table)
 
     console.print("🎈 [yellow]Selecione um item[/yellow]")
-
-while True:
-    init()
     try:
         number = int(console.input("👉 "))
         if number < 0 or number > lenn:
             console.print("👾 [bold][red]Error[/red][/bold] ~ [bold]Valor superior/inferior ao apresentado[/bold]")
+            clear()
         else:
             console.print(f":warning-emoji: [purple]Abrindo[/purple] ~ [yellow]{url.entries[number].title}[/yellow]")
             web.open(f"{url.entries[number].link}")
+            clear()
     except ValueError as VE:
         console.print("👹 [red]Error[/red] ~ [pink]Valor não suportado[/pink]") 
-        pass
+        clear()
