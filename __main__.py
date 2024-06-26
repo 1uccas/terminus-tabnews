fe416d2cb7e23e2a6036ee64158ecc131d804b1a
Luccas <nascimentolucas2164@gmail.com>
2024-06-26 16:48:38 -0300

    feat --> add function to exit()

--- __main__.py
+++ __main__.py
@@ -25,19 +25,22 @@ while True:
 
     console.print(table)
 
-    console.print("🎈 [yellow]Selecione um item[/yellow]")
+    console.print("🎈 [yellow]Selecione um item[/yellow]\n[red][00] - Para SAIR (x)[/red]")
     try:
         number = int(console.input("👉 "))
         if number < 0 or number > lenn:
             console.print("👾 [bold][red]Error[/red][/bold] ~ [bold]Valor superior/inferior ao apresentado[/bold]")
             sleep(1)
             clear()
+        elif number == 00:
+            console.print("🔓 [yellow]Saindo...[/yellow]")
+            exit()
         else:
             console.print(f":warning-emoji: [purple]Abrindo[/purple] ~ [yellow]{url.entries[number].title}[/yellow]")
             sleep(1)
             web.open(f"{url.entries[number].link}")
             clear()
-    except ValueError as VE:
+    except Exception as VE:
         console.print("👹 [red]Error[/red] ~ [pink]Valor não suportado[/pink]") 
         sleep(1)
         clear()
